eea4f895ae72c50c8ed4363043f33c183359e287
AnthonyPerez10 <anthony.perez10@utp.ac.pa>
2025-09-12 20:45:33 -0500

    Agregamos Programa 2 condicion Switch

---
+++ Tarea #3 - Platzy/Program2 - Swith/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Control de de Switch 
+
+//ID: 11
+
+int totalJugador = 23;
+String message = "";
+var totalDealer = 15;
+String SwitchControl = "21";
+
+//Juego Blackjack, juntar 21 pidiendo cartas en caso de tener menos
+switch(SwitchControl)
+{
+    case "Menu":
+        Console.WriteLine("Bienvenido al casino");
+        Console.WriteLine("Escriba 21, para jugar 21");
+        SwitchControl = Console.ReadLine();
+        break;
+    case "21":
+        if (totalJugador > totalDealer && totalJugador < 22)
+        {
+            message = "Venciste al Dealer, felicidades";
+        }
+        else if (totalJugador > 21)
+        {
+            message = "Perdiste vs Dealer, te pasaste de 21";
+        }
+        else if (totalJugador <= totalDealer)
+        {
+            message = "Perdiste vs el Dealer, lo siento";
+        }
+        else
+        {
+            message = "Condición no valida";
+        }
+
+        Console.WriteLine("El resultado es: " + message);
+        break;
+    default:
+        Console.WriteLine("Opcion no valida");
+        break;
+}
--- Tarea #3 - Platzy/Promam-IF/obj/Debug/net9.0/Promam-IF.AssemblyInfo.cs
+++ Tarea #3 - Platzy/Promam-IF/obj/Debug/net9.0/Promam-IF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Promam-IF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb3d6b08fa7f0a85102724a39b2e01edf7ac2dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Promam-IF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Promam-IF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
